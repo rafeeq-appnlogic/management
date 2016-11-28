7cdaba00d03c4998ff36be589ab4ea1289223bef
rafeeq-appnlogic <rafeeq@appnlogic.com>
2016-11-28 13:37:47 +0530

    smart edu setup

---
+++ application/views/footer.php
@@ -0,0 +1,26 @@
+<!-- Javascript files to load here -->
+<!-- <script src="<?php echo base_url(); ?>assets/plugins/jquery/jquery-1.9.1.min.js"></script> -->
+
+<!-- <script src="<?php echo base_url(); ?>assets/js/jquery-1.11.3.min.js"></script> -->
+<script src="<?php echo base_url(); ?>assets/js/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery/jquery-migrate-1.1.0.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-ui/ui/minified/jquery-ui.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<!-- <script src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script> -->
+
+<!--[if lt IE 9]>
+	<script src="<?php echo base_url(); ?>assets/crossbrowserjs/html5shiv.js"></script>
+	<script src="<?php echo base_url(); ?>assets/crossbrowserjs/respond.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/crossbrowserjs/excanvas.min.js"></script>
+<![endif]-->
+<script src="<?php echo base_url(); ?>assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-cookie/jquery.cookie.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/apps.min.js"></script>
+
+<script>
+		$(document).ready(function() {
+			App.init();
+		});
+	</script>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+	<meta content="" name="description" />
+	<meta content="CloudLogic" name="author" />
+	<title>Smart EDU</title>
+<!-- Css files to load here -->
+	<link href="<?php echo base_url(); ?>assets/plugins/jquery-ui/themes/base/minified/jquery-ui.min.css" rel="stylesheet" />
+	<link href="<?php echo base_url(); ?>assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="<?php echo base_url(); ?>assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="<?php echo base_url(); ?>assets/css/animate.min.css" rel="stylesheet" />
+	<link href="<?php echo base_url(); ?>assets/css/style-smartedu.css" rel="stylesheet" />
+	<link href="<?php echo base_url(); ?>assets/css/style-responsive.min.css" rel="stylesheet" />
+	<link href="<?php echo base_url(); ?>assets/css/theme/default-smartedu.css" rel="stylesheet" id="theme" />
+	<!-- <script src="<?php echo base_url(); ?>assets/plugins/pace/pace.min.js"></script> -->
+	
+	
+</head>
+<body>
+
+
